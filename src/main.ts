3ca565f6b9139d28c238099a55fa5ca49a6d645b
andrey-lawyer <andrey-1@ukr.net>
2024-02-08 21:46:05 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,12 @@ async function bootstrap() {
   });
 
   app.use((_, res, next) => {
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
     next();
   });
   app.useGlobalPipes(new ValidationPipe());
