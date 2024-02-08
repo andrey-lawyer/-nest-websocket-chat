348e2c36308fcc978fc210bf843a910b21076356
andrey-lawyer <andrey-1@ukr.net>
2024-02-08 20:53:17 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.use(
     session({
@@ -19,17 +19,17 @@ async function bootstrap() {
   //   origin: process.env.FRONT_URL || 'http://localhost:5173',
   //   credentials: true,
   // });
-  app.enableCors({
-    origin: [
-      'https://react-socket-chat-zeta.vercel.app',
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:5175',
-      process.env.FRONT_URL,
-    ],
-    // credentials: true,
-    credentials: process.env.FRONT_URL.includes('localhost') ? true : false,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'https://react-socket-chat-zeta.vercel.app',
+  //     'http://localhost:5173',
+  //     'http://localhost:5174',
+  //     'http://localhost:5175',
+  //     process.env.FRONT_URL,
+  //   ],
+  //   // credentials: true,
+  //   credentials: process.env.FRONT_URL.includes('localhost') ? true : false,
+  // });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => console.log('server listening on port ' + PORT));
 }
