c3ed2bf4d9d48e7ea106826c1aa5c7d150a511e8
andrey-lawyer <andrey-1@ukr.net>
2024-02-15 11:07:48 +0200

    fixed dependencies

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MemberService } from './member.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Member } from './member.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Member]),
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.PRIVATE_KEY || 'secret_key',
+        signOptions: { expiresIn: '24h' },
+      }),
+    }),
+  ],
   providers: [MemberService],
   exports: [MemberService],
 })
